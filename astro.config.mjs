16b45a0a1d968b024dabb40ce9d2d1036c30ddec
Andreas Arnesson <Andreas_osby@hotmail.com>
2025-05-07 14:53:11 +0200

    kopierat webtec

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,71 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 import starlight from '@astrojs/starlight';
+import starlightImageZoom from 'starlight-image-zoom'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://webtec-2024.github.io/website/',
+	base: '/website',
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+	},
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			plugins: [
+				starlightImageZoom(),
+			],
+			title: 'Kursen webtec',
+			favicon: 'favicon.png',
+			logo: {
+				//src: './src/assets/leaf_256x256.png',
+				src: '@assets/leaf_256x256.png',
+			},
+			customCss: [
+				'./src/styles/dbwebb.css',
+			],
+			editLink: {
+				baseUrl: 'https://github.com/webtec-2024/website/tree/main',
+			},
+			social: {
+				github: 'https://github.com/webtec-2024/website',
+			},
+			head: [
+				{
+				  tag: 'base',
+				  attrs: {
+					href: '/website/'
+				  }
+				}
+			],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Introduktion',
+					collapsed: true,
+					autogenerate: { directory: 'intro' },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Kursmoment',
+					autogenerate: { directory: 'kmom' },
+				},
+				{
+					label: 'Läromaterial',
+					collapsed: true,
+					autogenerate: { directory: 'laromaterial' },
+				},
+				{
+					label: 'Studieguide',
+					collapsed: true,
+					autogenerate: { directory: 'studieguide' },
+					/* items: [
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],*/
+				},
+				{
+					label: 'Övrigt',
+					collapsed: true,
+					autogenerate: { directory: 'ovrigt' },
 				},
 			],
 		}),
---
+++ src/content/docs/laromaterial/labbmiljo/index.md
@@ -0,0 +1,10 @@
+---
+title: "Installera kursens labbmiljö" 
+description: Instruktioner om hur du installerar kursens labbmiljö.
+sidebar:
+    order: 100
+---
+
+Labbmiljön bygger främst på verktyg som är fria och ofta erbjuds i form av öppen källkod.
+
+När val sker så försöker vi finna verktyg som finns på de vanligaste operativsystemen vilka är Windows, Mac och Linux.
